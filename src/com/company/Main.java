e6727e74c3e171d409f0ded8063c213d81fdc82a
Карина <crazyallmylife2014>
2021-09-28 16:29:21 +0300

    Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//Первое задание
+
+//        short a = -2000;
+//        long c = 200000025;
+//        int k = 5268;
+//        int o = 25;
+//        int div;
+//        float t = 85.3F;
+//        double db = 52.23;
+//        char i = 'l';
+//        byte b = 10;
+
+// Второе задание
+
+//        int a = 20;
+//        int b = 2;
+//        int k = 0;
+//        int c = a/b;
+//        System.out.println(c);
+//        System.out.println(a/k);
+
+// третье задание
+
+//        int i = 20;
+//        int k = 85;
+//        int e = ++i * ++k;
+//        if (k >= 85 & i > 90) {
+//            System.out.println("I less than 60");
+//        } else if (i < 40) {
+//            System.out.println("I more than 19");
+//        } else if (i < 92) {
+//            System.out.println("I less 92");
+//        } else {
+//            System.out.println("end");
+//        }
+
+//четвертое задание
+
+//        int a,b;
+//        a = 10;
+//        b = 25;
+//        int t = a/b;
+//        double c = (double) a/b;
+//
+//        if (c % 1 == 0) {
+//            System.out.println("Число делится без остатка: " + t);
+//        }else {
+//            System.out.println("Остаток: " + c);
+//        }
+//        System.out.println();
+
+// Пятое задание
+
+//        Scanner in = new Scanner(System.in);
+//        int a, b, k, l, d, n;
+//        n = in.nextInt();
+//        l = n / 10 % 10;
+//        k = n / 100 % 10;
+//        d = n % 10;
+//        b = n /1000;
+//        System.out.print(b + " тысяч " + k + " сотен " + l + " десятков " + d + " единиц " );
+
+
+// Шестое задание
+
+    int b= 128;
+    byte a= (byte) b;
+    int i = 10;
+    float t = 10f;
+    char symbol = 'a';
+    symbol++;
+        System.out.println(symbol);
+        System.out.println((int)symbol);
+        System.out.println(a);
+        System.out.println(t/0);
+        System.out.println(i/0);
+
+    }
+}
